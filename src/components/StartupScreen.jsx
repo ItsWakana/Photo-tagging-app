beb546c6b108f42e095b50cdf879c90562be0fae
itswakana <100365678+ItsWakana@users.noreply.github.com>
2023-06-19 19:33:23 +0100

    add comment for next stage

--- src/components/StartupScreen.jsx
+++ src/components/StartupScreen.jsx
@@ -5,10 +5,10 @@ const StartupScreen = () => {
 
     const { toggleGameState } = useContext(DataContext);
 
-
+    //TODO: ADD BACKGROUND IMAGE BELOW SO USER CAN SEE WHAT THEY WILL BE DOING.
     return (
         <div className="startup-menu">
-            <h1 className="text-white font-bold text-2xl">FIND THE CHARACTERS</h1>
+            <h1 className="text-white font-bold text-">FIND THE CHARACTERS</h1>
             <button onClick={toggleGameState} className=" rounded-md text-sm">
                 START GAME
             </button>
