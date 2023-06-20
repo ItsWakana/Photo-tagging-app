446a1df31fb80736dcbf929b03d9c8a1aace78dd
wakana <wakanawardrobe@gmail.com>
2023-06-20 12:28:46 +0100

    add firebase hosting setup

---
+++ src/firebaseSetup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV1OMHCFtW1c_3a04ljMySAWO2pQjyBsQ",
+  authDomain: "character-finder-3b6c2.firebaseapp.com",
+  projectId: "character-finder-3b6c2",
+  storageBucket: "character-finder-3b6c2.appspot.com",
+  messagingSenderId: "568076698828",
+  appId: "1:568076698828:web:4b380fd944b3e65ed86e69"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
