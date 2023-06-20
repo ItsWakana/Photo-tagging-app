b5114ccc29cf876df7e439fcbfde6abe70d0764c
wakana <wakanawardrobe@gmail.com>
2023-06-20 13:07:59 +0100

    add game thumbail to startup screen

--- src/components/StartupScreen.jsx
+++ src/components/StartupScreen.jsx
@@ -8,8 +8,9 @@ const StartupScreen = () => {
     //TODO: ADD BACKGROUND IMAGE BELOW SO USER CAN SEE WHAT THEY WILL BE DOING.
     return (
         <div className="startup-menu">
-            <h1 className="text-white font-bold text-">FIND THE CHARACTERS</h1>
-            <button onClick={toggleGameState} className=" rounded-md text-sm">
+            <h1 className="text-white font-normal text-xl">FIND THE CHARACTERS</h1>
+            <img className="mini-bg rounded-lg" src={`${import.meta.env.BASE_URL}images/universe-113-poster crop.jpg`} alt="" />
+            <button onClick={toggleGameState} className=" rounded-lg text-sm w-full">
                 START GAME
             </button>
         </div>
