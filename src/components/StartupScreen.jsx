56858665cfd9449db054f7a3193c5ad2e2dbb380
wakana <wakanawardrobe@gmail.com>
2023-06-26 11:08:13 +0100

    storage startup screen image on firebase

--- src/components/StartupScreen.jsx
+++ src/components/StartupScreen.jsx
@@ -1,19 +1,41 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { DataContext } from "../context/GameData";
+import { storage } from "../firebaseSetup";
+import { ref, getDownloadURL } from "firebase/storage";
 
 const StartupScreen = () => {
 
     const { toggleGameState, gameStarted } = useContext(DataContext);
+    const [imageUrl, setImageUrl] = useState(null);
 
     //TODO: ADD BACKGROUND IMAGE BELOW SO USER CAN SEE WHAT THEY WILL BE DOING.
 
+    useEffect(() => {
+
+        const getInitialImage = async () => {
+
+            const imageRef = ref(storage, 'images/universe-113-poster crop.jpg');
+
+            const url = await getDownloadURL(imageRef);
+
+            setImageUrl(url);
+        }
+
+        getInitialImage()
+    },[]);
+    
     return (
         // <div className="startup-menu">
 
         !gameStarted && (
             <div className={`startup-menu ${gameStarted ? 'invisible' : ''}`}>
                 <h1 className="text-white font-normal text-xl">FIND THE CHARACTERS</h1>
-                <img className="mini-bg rounded-lg" src={`${import.meta.env.BASE_URL}images/universe-113-poster crop.jpg`} alt="" />
+                {imageUrl ? (
+                    <img className="mini-bg rounded-lg" src={`${import.meta.env.BASE_URL}images/universe-113-poster crop.jpg`} alt="" />
+                ) : (
+                    <p>Loading...</p>
+                )}
+                {/* <img className="mini-bg rounded-lg" src={`${import.meta.env.BASE_URL}images/universe-113-poster crop.jpg`} alt="" /> */}
                 <button onClick={toggleGameState} className=" rounded-lg text-sm w-full p-2">
                     START GAME
                 </button>
